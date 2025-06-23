11ca85982371c7834b23fe198b7efede24f7b11d
whyyhyh <judynam03@gmail.com>
2025-06-23 22:57:09 +0900

    feat: Pretendard 웹폰트 적용

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,8 +2,8 @@
 import { Global, css } from '@emotion/react';
 
 export const GlobalStyles = () => (
-  <Global
-    styles={css`
+    <Global
+        styles={css`
       /* Reset CSS */
       *, *::before, *::after {
         margin: 0;
@@ -14,11 +14,17 @@ export const GlobalStyles = () => (
         width: 100%;
         height: 100%;
       }
+            body {
+        font-family: 'Pretendard', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
       a {
         text-decoration: none;
         color: inherit;
       }
       /* 필요하면 여기 추가로 스타일 초기화 */
     `}
-  />
+    />
 );
